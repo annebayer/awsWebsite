1b82b231a08d7a32e25f1f23d2f13e475a413242
Anne Bayer <anne.bayer@andrena.de>
2025-04-22 09:42:32 +0200

    Deactivate Routing

--- angular-website-aws/src/app/app.routes.ts
+++ angular-website-aws/src/app/app.routes.ts
@@ -1,19 +1,17 @@
 import { Routes } from '@angular/router';
-import {TestComponent} from './unterseite-test/test.component';
-import {StartseiteComponent} from './startseite/startseite.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/startseite',
-    pathMatch: 'full',
-  },
-  {
-    path: 'test',
-    component: TestComponent
-  },
-  {
-    path: '**',
-    component: StartseiteComponent
-  },
+  // {
+  //   path: '',
+  //   redirectTo: '/startseite',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: 'test',
+  //   component: TestComponent
+  // },
+  // {
+  //   path: '**',
+  //   component: StartseiteComponent
+  // },
 ];
