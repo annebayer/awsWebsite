f168d5e1641ff547e1c6bc50aff7bd376d0ed3d8
Anne Bayer <anne.bayer@andrena.de>
2025-04-22 09:57:26 +0200

    Reactivate Routing

--- angular-website-aws/src/app/app.routes.ts
+++ angular-website-aws/src/app/app.routes.ts
@@ -1,17 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {TestComponent} from './test/test.component';
+import {StartseiteComponent} from './startseite/startseite.component';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: '/startseite',
-  //   pathMatch: 'full',
-  // },
-  // {
-  //   path: 'test',
-  //   component: TestComponent
-  // },
-  // {
-  //   path: '**',
-  //   component: StartseiteComponent
-  // },
+  {
+    path: '',
+    redirectTo: '/startseite',
+    pathMatch: 'full',
+  },
+  {
+    path: 'test',
+    component: TestComponent
+  },
+  {
+    path: '**',
+    component: StartseiteComponent
+  },
 ];
