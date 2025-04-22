04c2653429c997d952308c80b377125cc5a0a871
Anne Bayer <anne.bayer@andrena.de>
2025-04-22 08:31:41 +0200

    First empty pages

--- angular-website-aws/src/app/app.routes.ts
+++ angular-website-aws/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {TestComponent} from './unterseite-test/test.component';
+import {StartseiteComponent} from './startseite/startseite.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/startseite',
+    pathMatch: 'full',
+  },
+  {
+    path: 'test',
+    component: TestComponent
+  },
+  {
+    path: '**',
+    component: StartseiteComponent
+  },
+];
