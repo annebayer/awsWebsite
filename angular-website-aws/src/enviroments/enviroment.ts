92d388385543187fef3d0327ec9a9e40c8dcbd13
Anne Bayer <anne.bayer@andrena.de>
2025-05-06 14:08:37 +0200

    Add Secret

---
+++ angular-website-aws/src/enviroments/enviroment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'WIRD_DURCH_AMPLIFY_ERSETZT',
+  webinyUrl: "https://d1y6qt5lrbb18s.cloudfront.net/cms/read/de-DE",
+  webinyToken: "Nachfragen" // Blur when you push to production
+};
