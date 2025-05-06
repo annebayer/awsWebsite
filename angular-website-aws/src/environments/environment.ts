c3a0d44d9b851c0442af0ac7851dc3daaa76b7a3
Anne Bayer <anne.bayer@andrena.de>
2025-05-06 16:49:20 +0200

    Make enviroment more concrete

--- angular-website-aws/src/environments/environment.ts
+++ angular-website-aws/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'WIRD_DURCH_AMPLIFY_ERSETZT',
   webinyUrl: "https://d1y6qt5lrbb18s.cloudfront.net/cms/read/de-DE",
-  webinyToken: "Nachfragen" // Blur when you push to production
+  webinyToken: "WIRD_IN_AMPLIFY_YML_ERSETZT"
 };
